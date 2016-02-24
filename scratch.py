24c67ea8e293ac86ca1e97fde7343f77ac6f081b
mikepaxton <mike.paxton@me.com>
2016-02-23 16:43:14 -0800

    Working on implementing MySQL database

--- scratch.py
+++ scratch.py
@@ -1,9 +1,11 @@
 import MySQLdb
 
-dbAddress = 'localhost'
-dbUser = 'rpi'
-dbPassword = '2Fast4uX2'
-dbName = 'greenhouse'
 
-con = MySQLdb.connect(host=dbAddress, user=dbUser, passwd=dbPassword, db=dbName)
-#c = con.cursor()
+while True:
+    dbAddress = 'localhost'
+    dbUser = 'rpi'
+    dbPassword = '2Fast4uX2'
+    dbName = 'greenhouse'
+
+    con = MySQLdb.connect(host=dbAddress, user=dbUser, passwd=dbPassword, db=dbName)
+    #c = con.cursor()
