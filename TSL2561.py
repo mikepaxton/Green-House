642b5c8516e654e8e70d344d6c1e515022090ae2
mikepaxton <mike.paxton@me.com>
2016-02-23 20:36:48 -0800

    Working with TSL2561 lux code

--- TSL2561.py
+++ TSL2561.py
@@ -16,7 +16,7 @@ from Adafruit_I2C import Adafruit_I2C
 class TSL2561:
     #i2c = None
 
-    def __init__(self, address=0x39, debug=1, pause=0.8):
+    def __init__(self, address=0x39, debug=0, pause=0.8):
         self.i2c = Adafruit_I2C(address)
         self.address = address
         self.pause = pause
