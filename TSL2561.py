ba9c7e44cb10858a833966d4aea76525b9c50eda
mikepaxton <mike.paxton@me.com>
2016-02-23 20:23:00 -0800

    Working with TSL2561 lux code

--- TSL2561.py
+++ TSL2561.py
@@ -14,7 +14,7 @@ from Adafruit_I2C import Adafruit_I2C
 # TODO: Strip out values into constants.
 
 class TSL2561:
-    i2c = None
+    #i2c = None
 
     def __init__(self, address=0x39, debug=0, pause=0.8):
         self.i2c = Adafruit_I2C(address)
