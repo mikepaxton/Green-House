a6f8decbd7024707d2c6496f8cca70ce0efde908
Mike Paxton <mike.paxton@me.com>
2016-03-11 03:10:44 -0800

    Added both exhaust and circulation fans. Added email message service. Incorporated dbPort into config file and code. Cleaned up various comments and code. Moved several code blocks around for better code organization.

--- main.py
+++ main.py
@@ -20,43 +20,52 @@ from Subfact_ina219 import INA219
 from TSL2561 import TSL2561
 from ConfigParser import SafeConfigParser
 import os
+import sys
 import MySQLdb
 import RPi.GPIO as GPIO
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 # TODO: Create a LCD control interface using tkinter or pygame
 # TODO: Incorporate Python logging Module into controls
 # TODO: Find a logging website other than io.adafruit for greater logging capabilities
 # TODO: Work with the TSL2561 light sensor to check for data accuracy
-# TODO: Make dbPort functional
-# TODO: Incorporate two fans into code which should kick on when temp get to high
+# TODO: Devise a means of checking the battery state before operating the fans
 
 
 # Global stuff
+tsl = TSL2561()
 config = SafeConfigParser()
 config.read('config.cfg')
 interval = config.get('defaults', 'interval')  # Get sensor updating interval
 debug = config.getboolean('defaults', 'debug')  # debug print to console
 ADAFRUIT_IO_KEY = config.get('defaults', 'aio_key')  # Import Adafruit aio Key
 aio = Client(ADAFRUIT_IO_KEY)
-DHT_TYPE = Adafruit_DHT.DHT22  # Get DHT Pin number
+DHT_TYPE = Adafruit_DHT.DHT22
 DHT_PIN = config.get('defaults', 'dht_pin')
-mysqlUpdate = config.getboolean('defaults',
-                                'mysqlUpdate')  # Check if using MySQL database
-tsl = TSL2561()
-temp_threshold = config.get('defaults', 'fan_on')
-temp_norm = config.get('defaults', 'fan_off')
+mysqlUpdate = config.getboolean('database', 'mysqlUpdate')  # Are we using database?
+max_cpu_temp = config.get('defaults', 'max_cpu_temp')
+temp_threshold = config.get('fans', 'exhaust_fan_on')
+temp_norm = config.get('fans', 'exhaust_fan_off')
+circulate_temp = config.get('fans', 'circulate_temp')
+message_service = config.getboolean('email', 'send_email')
+
 
 # Setup and initiate fans on GPIO pins.  Fans should be connected to a relay board.
 GPIO.setmode(GPIO.BCM)
-fans = [4]
-for i in fans:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, GPIO.HIGH)
+exhaust_fan = config.get('fans', 'exhaust_fan_pin')
+GPIO.setup(exhaust_fan, GPIO.OUT)
+GPIO.output(exhaust_fan, GPIO.HIGH)
+circulate_fan = config.get('fans', 'circulate_fan_pin')
+GPIO.setup(circulate_fan, GPIO.OUT)
+GPIO.output(circulate_fan, GPIO.HIGH)
 
 
 def checkDebug(message):
     """ Check for debug status on config file. Function can be called to print debug
-    message.
+    message.  The only parameter is message which you pass a message string.
     """
     if debug == True:
         print(message)
@@ -69,20 +78,23 @@ checkDebug('Using Database: ' + str(mysqlUpdate))
 checkDebug('Update interval in seconds: ' + str(interval))
 checkDebug('Fan ON Temp: ' + str(temp_threshold))
 checkDebug('Fan OFF Temp: ' + str(temp_norm))
+checkDebug('Send email messages: ' + str(message_service))
 
 
+# Main Functions
 def dbUpdate():
     """ Open a connection to mysql database using the MySQLdb library.  The database
     can be either local or remote.  If remote you must change both the dbaddress and
     the port number to that of remote. Changed the database connection information
     over to the config file.  Note: dbPort is not implemented in code yet.
     """
-    dbAddress = config.get('defaults', 'dbAddress')
-    dbUser = config.get('defaults', 'dbUser')
-    dbPassword = config.get('defaults', 'dbPassword')
-    dbName = config.get('defaults', 'dbName')
-    # dbPort = config.get('defaults', 'dbPort')
-    con = MySQLdb.connect(host=dbAddress, user=dbUser, passwd=dbPassword, db=dbName)
+    dbAddress = config.get('database', 'dbAddress')
+    dbUser = config.get('database', 'dbUser')
+    dbPassword = config.get('database', 'dbPassword')
+    dbName = config.get('database', 'dbName')
+    dbPort = config.get('database', 'dbPort')
+    con = MySQLdb.connect(host=dbAddress, Port=dbPort, user=dbUser, passwd=dbPassword,
+                          db=dbName)
     c = con.cursor()
 
     date = datetime.datetime.now()
@@ -96,12 +108,39 @@ def dbUpdate():
     con.close()
 
 
-# Main Functions
+def send_email(subject, message):
+    """ Function to allow sending an email message for various alerts. Two parameters
+    need to be passed which are the 'subject' of the message and the 'message' itself.
+    The rest of the built-in parameters are defined from the config.cfg file under the
+    [email] section and must be set to your needs in order for the function to work
+    correctly.
+    :param subject:
+    :param message:
+    """
+    email_from = config.get('email', 'email_from')
+    email_to = config.get('email', 'email_to')
+    email_password = config.get('email', 'email_password')
+    msg = MIMEMultipart()
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = subject
+    body = message
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP(config.get('email', 'smtp_server'))
+    server.ehlo()
+    server.starttls()
+    server.login(email_from, email_password)
+    text = msg.as_string()
+    server.sendmail(email_from, email_to, text)
+    server.quit()
+
+
 def getCPUtemp():
     """Function used to grab RPi CPU Temp and return CPU temperature as a character
     string"""
     res = os.popen('vcgencmd measure_temp').readline()
-    return (res.replace("temp=", "").replace("'C\n", ""))
+    return res.replace("temp=", "").replace("'C\n", "")
 
 
 def cels_fahr(cels):
@@ -210,18 +249,48 @@ try:
             checkDebug('Database Update Skipped')
 
         # Check both temp ranges from config file.  The dht_temp must be equal to or
-        # higher but not lower than the norm for fans to run.
+        # higher than threshold but not lower than the norm for exhaust to run.
         # Must convert both temp ranges to an integer as they are brought from config
         # file as strings.
         if dht_temp >= int(temp_threshold) and dht_temp > int(temp_norm):
-            GPIO.output(fans, GPIO.LOW)
-            checkDebug('Fans are ON')
+            GPIO.output(exhaust_fan, GPIO.LOW)
+            checkDebug('Exhaust fans is ON')
         else:
-            GPIO.output(fans, GPIO.HIGH)
-            checkDebug('Fan are OFF')
+            GPIO.output(exhaust_fan, GPIO.HIGH)
+            checkDebug('Exhaust fan is OFF')
 
+        # Check dht_temp against circulate_fan temp and if if need be turn on circulate
+        # fan.
+        if dht_temp >= circulate_temp:
+            GPIO.output(circulate_fan, GPIO.LOW)
+            checkDebug('Circulation fan is ON')
+        else:
+            GPIO.output(circulate_fan, GPIO.HIGH)
+            checkDebug('Circulate fan is OFF')
+
+        # Shutdown the Raspberry Pi if the cpu temp gets to hot.  If message_service is
+        # set to True an email message will be sent out so long as there is an Internet
+        # connection available. The system will wait for X number of seconds to send
+        # the message before shutting down.
+        if cpu_temp >= max_cpu_temp:
+            if message_service == True:
+                message = "The CPU temperature has reached the maximum allowed set in " \
+                          " the config file so the system is being shutdown.  This " \
+                          "means the fans are no longer working and the plants in the " \
+                          "greenhouse are in danger. /n  Please check the system as " \
+                          "soon as possible!"
+                send_email('Shutdown', message)
+                checkDebug('CPU Temp to high, system is shutting down.')
+                time.sleep(30)
+
+            GPIO.cleanup()
+            os.system('shutdown -h now')
+        else:
+            continue
 
         time.sleep(float(interval))
+
 except KeyboardInterrupt:
     GPIO.cleanup()
     checkDebug('Interrupt caught, GPIO.cleanup ran!')
+    sys.exit(0)
