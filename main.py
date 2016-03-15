dad9c75389b4ad38f4de22d500b6aa4b9c69ee6d
mikepaxton <mike.paxton@me.com>
2016-03-15 16:34:14 -0700

    Fan control

--- main.py
+++ main.py
@@ -56,12 +56,12 @@ message_service = config.getboolean('email', 'send_email')
 
 # Setup and initiate fans on GPIO pins.  Fans should be connected to a relay board.
 GPIO.setmode(GPIO.BCM)
-exhaust_fan = config.get('fans', int('exhaust_fan_pin'))
-GPIO.setup(exhaust_fan, GPIO.OUT)
-GPIO.output(exhaust_fan, GPIO.HIGH)
+exhaust_fan = config.get('fans', 'exhaust_fan_pin')
+GPIO.setup(int(exhaust_fan), GPIO.OUT)
+GPIO.output(int(exhaust_fan), GPIO.HIGH)
 circulate_fan = config.get('fans', int('circulate_fan_pin'))
-GPIO.setup(circulate_fan, GPIO.OUT)
-GPIO.output(circulate_fan, GPIO.HIGH)
+GPIO.setup(int(circulate_fan), GPIO.OUT)
+GPIO.output(int(circulate_fan), GPIO.HIGH)
 
 
 def checkDebug(message):
