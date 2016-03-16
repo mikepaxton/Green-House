0a7c19e52c335d5d97b0ed5b8a4905b73699eed5
mikepaxton <mike.paxton@me.com>
2016-03-15 22:00:42 -0700

    Testing BME280 temp/humidity sensor

--- scratch.py
+++ scratch.py
@@ -7,12 +7,12 @@ pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 humidity = sensor.read_humidity()
 
-temp = cels_fahr(degrees
-                 )
 def cels_fahr(cels):
     """Function takes in celsius temperature and returns temp in Fahrenheit"""
     temp = cels * 9.0 / 5 + 32
     return temp
+
+temp = cels_fahr(degrees)
 
 print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
 print 'Temp      = {0:0.3f} deg C'.format(temp)
